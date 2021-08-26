c33a648cfa16a3cdcc131617669a1532ac47fb4b
tedkorho <teo.i.korhonen@helsinki.fi>
2021-08-26 15:16:28 +0300

    Fixed issues w/modelling; TODO slow as heck

--- src/flare_energy.py
+++ src/flare_energy.py
@@ -44,11 +44,10 @@ def Model_Davenport(t, A1, A2, k1, k2, a1, a2, a3, a4):
     Assumes the flare peaks at t=0.
     """
 
-    return np.piecewise([t, [t < 0., t>= 0.],
+    return np.piecewise(t, [t < 0., t>= 0.],
             [lambda tp: a1*tp + a2*tp**2 + a3*tp**3 + a4*tp**4 + (A1+A2),
             lambda tp: A1*np.exp(-k1*tp) + A2*np.exp(-k2*tp)
             ]
-        ]
     )
 
 def Model_Davenport_adapted(t, A1, A2, k1, k2):
@@ -58,7 +57,7 @@ def Model_Davenport_adapted(t, A1, A2, k1, k2):
 
     return np.piecewise(t, [t < 0, t>= 0],
         [lambda tp: 0.1,
-        lambda tp: A1*np.exp(-k1*tp)) + A2*np.exp(-k2*tp)
+        lambda tp: A1*np.exp(-k1*tp) + A2*np.exp(-k2*tp)
         ]
     )
 
@@ -73,9 +72,8 @@ def Model_flare(Fres, t, model="exp"):
     some helper values are provided in the beginning.
     """
     
-    interpol = interp1d(t, Fres)
-    
-    x = np.linspace(np.min(t), np.max(t), 0.1/(24.*60.))
+    interpol = interp1d(t, Fres, fill_value="extrapolate")
+    x = np.arange(t[0], t[-1], 1.0/(24.*60.))
     y = interpol(x)
     maxx = x[np.argmax(y)]
     maxy = np.max(y)
@@ -86,10 +84,10 @@ def Model_flare(Fres, t, model="exp"):
     t1 = x[0]
     t2 = maxx
     for i in range(len(x)):
-        if ctr = 0 and y[i] > 0.5*maxy:
+        if ctr == 0 and y[i] > 0.5*maxy:
             ctr = 1
             t1 = x[i]
-        if ctr = 1 and y[i] < 0.5*maxy:
+        if ctr == 1 and y[i] < 0.5*maxy:
             t2 = x[i]
             break
 
@@ -114,18 +112,17 @@ def Model_flare(Fres, t, model="exp"):
         ]
     
     if model == "Davenport":
-        f = Model_Davenport(t, A1, A2, k1, k2, a1, a2, a3, a4)
+        f = Model_Davenport #(t, A1, A2, k1, k2, a1, a2, a3, a4)
         
         # Initial guess: sharper exp higher, maximum at t0 
         
         maxindex = np.argmax(y)
-        thalf = 0. #TODO the t_1/2 thing from Davenport
 
         initialguess = [
-            -np.log(0.05) / (x[-FLARE_PAD] - x[maxindex]),
-            -np.log(0.1) / (x[-FLARE_PAD] - x[maxindex]),
             3./4., 
             1./4., 
+            -np.log(0.05) / (x[-FLARE_PAD] - x[maxindex]),
+            -np.log(0.1) / (x[-FLARE_PAD] - x[maxindex]),
             1., 1., 1., 1.,
         ]
 
@@ -149,6 +146,10 @@ def Model_flare(Fres, t, model="exp"):
     Fres_model = f(x, *popt)*maxy
     time = x*thalf + maxx
     
+    plt.plot(time, Fres_model)
+    plt.plot(t, Fres)
+    plt.show()
+
     return (maxy, time, Fres_model)
 
 
@@ -260,21 +261,19 @@ def E_flare(iamps, itimes, T_flare, LpS, LpF, R_star, model="none"):
         Fmodel = Model_flare(amps, times, "exp")
         impulsiveness = Fmodel[0]/2
         lumin = L_f(Fmodel[2])
-        # energy = L_f(Fmodel[0][0]*Fmodel[0][1]) Work out the units!!!
         energy = simps(lumin, Fmodel[1])
 
     elif model == "Davenport":
         Fmodel = Model_flare(amps, times, "Davenport")
         impulsiveness = Fmodel[0]/2
         lumin = L_f(Fmodel[2])
-        energy = simps(L_f(lumin, Fmodel[1]) # same here!
-
+        energy = simps(lumin, Fmodel[1]) # same here!
 
     elif model == "Davenport_adapted":
         Fmodel = Model_flare(amps, times, "Davenport_adapted")
         impulsiveness = Fmodel[0]/2
         lumin = L_f(Fmodel[2])
-        energy = simps(L_f(lumin, Fmodel[1]) # same here!
+        energy = simps(lumin, Fmodel[1]) # same here!
 
     else:
         impulsiveness = np.max(amps)/2
@@ -326,7 +325,7 @@ def main():
         elif in_flare:
             amps = pdcflux_ratio[i - flare_dur - FLARE_PAD : i + FLARE_PAD]
             ts = time[i - flare_dur - FLARE_PAD : i + FLARE_PAD] * DAY
-            (i_f, E_f) = E_flare(amps, ts, T_FLARE, LpS, LpF, R_STAR, model="exp")
+            (i_f, E_f) = E_flare(amps, ts, T_FLARE, LpS, LpF, R_STAR, model="Davenport")
             flare_energies.append(E_f / ERG)
             flare_impulses.append(i_f)
             flare_times.append(time[i - flare_dur])
